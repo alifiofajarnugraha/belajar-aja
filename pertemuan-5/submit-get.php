d452eab4d3a09dc9d08f612d4870d57b7fe610d6
alifiofajarnugraha <148094334+alifiofajarnugraha@users.noreply.github.com>
2024-10-20 12:46:30 +0700

    Delete pertemuan-5 directory

--- pertemuan-5/submit-get.php
+++
@@ -1,9 +0,0 @@
-<?php
-$name = $_POST ['nama'];
-$umur = $_POST ['nama'];
-$alamat = $_POST ['gatau'];
-
-echo "nama anda adalah". $nama;
-echo "umur anda adalah". $umur;
-echo "alamat anda adalah". $alamat;
-?>
